6cd18c87f0dfe47d1bd2e76c03ce11965c8534f1
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2024-05-20 22:49:43 +0530

    third  commit

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
